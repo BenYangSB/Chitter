8500e44f3a2f2e5c887e276d7e25bba31d174104
BenYangSB <72826356+BenYangSB@users.noreply.github.com>
2021-03-22 22:04:10 -0700

    feed fixes, display ingredients in an ordered list

--- chitter/src/components/exercises-list.component.js
+++ chitter/src/components/exercises-list.component.js
@@ -2,14 +2,37 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
+class Lst extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div>
+
+      <h>Ingredients</h>
+      {
+      this.props.ingList.map(ing=>{
+        return <li>{ing}</li>
+      })
+      }
+    </div>
+
+  }
+}
 const Exercise = props => (
 
   <div class = "card" id = "recipe">
 
-      <p>{props.exercise.username}</p>
-      <p>{props.exercise.description}</p>
-      <p>{props.exercise.duration}</p>
-      <p>{props.exercise.ingredients}</p>
+
+      <h>{props.exercise.username}</h>
+      <p>Dish : {props.exercise.description}</p>
+      <p>Time to make : {props.exercise.duration} minutes</p>
+
+      <Lst ingList = {props.exercise.ingredients}></Lst>
+
+      <br></br>      
       <p>{props.exercise.date.substring(0,10)}</p>
       { 
         props.exercise.userKey == props.currentKey && 
@@ -21,6 +44,7 @@ const Exercise = props => (
   </div>
 
 )
+
 
 export default class ExercisesList extends Component {
   constructor(props) {
@@ -67,6 +91,17 @@ export default class ExercisesList extends Component {
 
   }
 
+  ingList(ingredients){
+    if(ingredients == undefined)
+      return;
+
+    console.log("HERE");
+    console.log(ingredients);
+    ingredients.map(ing => {
+      return <li>{ing}</li>
+    })
+  }
+
   deleteExercise(id) {
     axios.delete('http://localhost:5000/exercises/'+id)
       .then(response => { console.log(response.data)});
@@ -82,7 +117,7 @@ export default class ExercisesList extends Component {
         return;
       console.log("HERE");
       console.log(currentexercise )
-      return <Exercise currentKey = {this.props.currentUserKey} exercise={currentexercise} deleteExercise={this.deleteExercise} key={currentexercise._id}/>;
+      return <Exercise ingList = {this.ingList()} currentKey = {this.props.currentUserKey} exercise={currentexercise} deleteExercise={this.deleteExercise} key={currentexercise._id}/>;
     })
   }
 

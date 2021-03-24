f2f802d9c6e6bca287152525d840ade15be9cfb4
BenYangSB <72826356+BenYangSB@users.noreply.github.com>
2021-03-24 11:29:12 -0700

    css changes

--- chitter/src/components/create-exercise.component.js
+++ chitter/src/components/create-exercise.component.js
@@ -88,7 +88,7 @@ export default class CreateExercise extends Component {
 
   render() {
     return (
-    <div>
+    <div class = "createRecipe">
       <h3>Post New Recipe!</h3>
       <form onSubmit={this.onSubmit}>
         <div className="form-group"> 
